64e042cba0b92d9e2192af4d12bcf51f97176ec4
Suman A <sumangowdaarbi@gmail.com>
2024-09-25 22:21:12 +0530

    25 September commit

--- src/main/java/Task/ArmstsrongNumberExample.java
+++ src/main/java/Task/ArmstsrongNumberExample.java
@@ -1,4 +1,25 @@
 package Task;
 
 public class ArmstsrongNumberExample {
+    public static void main(String[] args) {
+        int number=371;
+
+        int originalNumber=number;
+        int remainder, result=0;
+
+
+        while (originalNumber!=0){
+            remainder=originalNumber%10;
+            result+=Math.pow(remainder,3);
+
+            originalNumber=originalNumber/10;
+        }
+
+        if(result==number){
+            System.out.println(number+" is a Amstrong number");
+        }else{
+            System.out.println(number+" is a not Amstrong number");
+        }
+
+    }
 }
