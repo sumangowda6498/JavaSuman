12294d144c3f0098816abb733e8004d99ce9e90e
Suman A <sumangowdaarbi@gmail.com>
2024-10-30 20:01:44 +0530

    30 October

---
+++ src/main/java/Task/ReverseNumber.java
@@ -0,0 +1,12 @@
+package Task;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int num=12345, reversed=0;
+        while(num!=0){
+            reversed=reversed*10 + num%10;
+            num=num/10;
+        }
+        System.out.println(reversed);
+    }
+}
