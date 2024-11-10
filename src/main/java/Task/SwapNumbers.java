5051e7f048f0c914fffe2a668033842129e7e1a3
Suman <sumangowdaarbi@gmail.com>
2024-11-10 23:08:13 +0530

    5 november commit

---
+++ src/main/java/Task/SwapNumbers.java
@@ -0,0 +1,14 @@
+package Task;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a=5, b=8;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+    }
+
+}
