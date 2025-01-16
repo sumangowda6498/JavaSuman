287b42aab9b9fa287a2711aad6b695e823c88dcd
Suman A <sumangowdaarbi@gmail.com>
2025-01-16 20:01:57 +0530

    Commit 17 January 2025 Hash set

---
+++ src/main/java/Clolections_List/Arralist_Exercise.java
@@ -0,0 +1,30 @@
+package Clolections_List;
+import java.util.ArrayList;
+public class Arralist_Exercise {
+    public static void main(String[] args) {
+        ArrayList<String> arl=new ArrayList<String>();
+        arl.add("apple");
+        arl.add("banana");
+        arl.add("cherry");
+        arl.add("mango");
+        arl.add("apple");
+        System.out.println(arl);
+        int index=arl.indexOf("apple");
+        arl.remove(index);
+        System.out.println(arl.size());
+
+
+        boolean flag=arl.contains("orange");
+
+        System.out.println(flag);
+        int val=0;
+        while (arl.size() > val) {
+
+            // Printing the element which holds above
+            // condition true
+            System.out.println(arl.get(val));
+            val++;
+        }
+
+    }
+}
--- src/main/java/Clolections_List/Arraylist.java
+++ src/main/java/Clolections_List/Arraylist.java
@@ -2,6 +2,10 @@ package Clolections_List;
 import java.util.ArrayList;
 
 public class Arraylist {
+    // can accept duplicate values
+    // ArraList,LinkedList,vector- Implementing List interface
+    // array have fixed size where as arraylist can grow dynamicaly
+    // you can access and insert any value in any index
     public static void main(String[] args) {
         ArrayList<Integer> arl=new ArrayList<Integer>();
         arl.add(3);
@@ -13,6 +17,8 @@ public class Arraylist {
         System.out.println(arl);
         System.out.println(arl.get(0));
         System.out.println(arl.contains(4));
+        System.out.println(arl.indexOf(4));
+        System.out.println(arl.size());
 
     }
 }
