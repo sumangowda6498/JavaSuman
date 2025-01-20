7e205a4a0ae315394a2b2992a3be4e55073ccaa8
Suman A <sumangowdaarbi@gmail.com>
2025-01-20 20:07:24 +0530

    Commit 20 January 2025 Task

---
+++ src/main/java/Task/Collectiontask_Amazon_repeatedNumberArray.java
@@ -0,0 +1,30 @@
+package Task;
+
+import java.util.ArrayList;
+
+public class Collectiontask_Amazon_repeatedNumberArray {
+    public static void main(String[] args) {
+
+        int a[]={4,5,5,5,6,4,6,7,4,4};
+
+        ArrayList<Integer> al=new ArrayList<Integer>();
+        for(int i=0;i<a.length;i++){
+            int k=0;
+            if(!al.contains(a[i])){
+                al.add(a[i]);
+                k++;
+
+                for(int j=i+1;j<a.length;j++){
+                    if(a[i]==a[j]){
+                        k++;
+                    }
+                }
+                System.out.println(a[i]+":"+k+" time");
+            }else{i++;}
+
+        }
+
+
+
+    }
+}
