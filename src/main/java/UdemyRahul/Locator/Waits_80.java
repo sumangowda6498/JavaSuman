a33083bddb820f8a3d312fa64284459771d3cf42
Suman A <sumangowdaarbi@gmail.com>
2025-04-24 19:50:15 +0530

    Commit 24 April wait udemy rakesh 2025 #82

--- src/main/java/UdemyRahul/Locator/Waits_80.java
+++ src/main/java/UdemyRahul/Locator/Waits_80.java
@@ -4,14 +4,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Waits_80 {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.edge.driver","resources/msedgedriver.exe");
         WebDriver driver=new EdgeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);//impilcite wait
+        //Explicite wait
+        // wait=new WebDriverWait(driver,5);
+        driver.manage().window().maximize();
         String[] itemsNeeded={"Cucumber","Carrot"};
 
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
@@ -19,7 +27,15 @@ public class Waits_80 {
         addItems(driver,itemsNeeded);
         driver.findElement(By.cssSelector("img[alt='Cart']")).click();
         driver.findElement(By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]")).click();
+
+       // wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input.promoCode")));//Explicite wait
+
         driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.cssSelector("button.promobtn")).click();
+
+       // wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));//Explicite wait
+
+        System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
     }
 
     private static void addItems(WebDriver driver, String[] itemsNeeded) {
