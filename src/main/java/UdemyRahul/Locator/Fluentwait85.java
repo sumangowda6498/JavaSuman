56d865b5f02e489c7356e8e166a746231f3f065d
Suman A <sumangowdaarbi@gmail.com>
2025-04-25 19:10:03 +0530

    Commit 24 April wait udemy rakesh 2025 #85

---
+++ src/main/java/UdemyRahul/Locator/Fluentwait85.java
@@ -0,0 +1,29 @@
+package UdemyRahul.Locator;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
+import java.util.function.Function;
+
+public class Fluentwait85 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.edge.driver","resources/msedgedriver.exe");
+        WebDriver driver=new EdgeDriver();
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+        driver.findElement(By.cssSelector("[id='start'] button")).click();
+
+        Wait<WebDriver> wait=new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(30)).pollingEvery(Duration.ofSeconds(3)).ignoring(NoSuchElementException.class);
+
+        WebElement helloworld= wait.until(new Function<WebDriver, WebElement>(){
+            public WebElement apply(WebDriver driver){
+          return driver.findElement(By.cssSelector("[id='finish'] h4"));
+        }
+        });
+        System.out.println(driver.findElement(By.cssSelector("[id='finish'] h4")).isDisplayed());
+}}
