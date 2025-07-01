3c2160456c3933b327927ef4b7d4481b30d5e993
Suman <sumangowdaarbi@gmail.com>
2025-07-01 23:21:22 +0530

    Commit 01 July Taskwu

--- src/main/java/UdemyRahul/Locator/CalenderUI_practiseSite_98.java
+++ src/main/java/UdemyRahul/Locator/CalenderUI_practiseSite_98.java
@@ -2,13 +2,19 @@ package UdemyRahul.Locator;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+
+import java.util.List;
 
 public class CalenderUI_practiseSite_98 {
     public static void main(String[] args) {
-        String monthNumber="6";
+        String monthNumber="7";
         String date="17";
         String year="2027";
+
+        String[] expectedList={monthNumber,date,year};
 
 
         System.setProperty("webdriver.edge.driver","resources/msedgedriver.exe");
@@ -22,10 +28,18 @@ public class CalenderUI_practiseSite_98 {
         driver.findElement(By.cssSelector(".react-calendar__navigation__label")).click();
 
         driver.findElement(By.xpath("//button[text()='"+year+"']")).click();
+        driver.findElement(By.xpath("//abbr[text()='July']")).click();
 
-        driver.findElements(By.cssSelector(".react-calendar__tile react-calendar__year-view__months__month")).get(Integer.parseInt(monthNumber)-1);
+        //driver.findElements(By.cssSelector(".react-calendar__tile react-calendar__year-view__months__month")).get(Integer.parseInt(monthNumber)-1).click();
 
         driver.findElement(By.xpath("//abbr[text()='"+date+"']")).click();
+
+        List<WebElement> actualList=driver.findElements(By.cssSelector(".react-date-picker__inputGroup__input"));
+
+        for (int i=0;i<actualList.size();i++){
+            System.out.println(actualList.get(i).getAttribute("value"));
+            System.out.println((Assert.assertEquals(actualList.get(i).getAttribute("value"),expectedList[i]));
+        }
 
     }
 }
