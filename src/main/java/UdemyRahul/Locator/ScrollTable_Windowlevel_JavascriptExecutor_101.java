c227746bcef0f9f411a1386240e83f7b1f6b9d4b
Suman <sumangowdaarbi@gmail.com>
2025-07-09 23:24:38 +0530

    Commit 09 July Taskwu

--- src/main/java/UdemyRahul/Locator/ScrollTable_Windowlevel_JavascriptExecutor_101.java
+++ src/main/java/UdemyRahul/Locator/ScrollTable_Windowlevel_JavascriptExecutor_101.java
@@ -1,10 +1,13 @@
 package UdemyRahul.Locator;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import java.lang.annotation.Documented;
+import java.util.List;
 
 public class ScrollTable_Windowlevel_JavascriptExecutor_101 {
     public static void main(String[] args) throws InterruptedException {
@@ -18,5 +21,12 @@ Thread.sleep(10000);
         js.executeScript("windows.scrollBy(0,500)");
         Thread.sleep(3000);
         js.executeScript("document.quesrySelector('.tableFixHead').scrollTop=5000");
+
+        List<WebElement> values=driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
+int sum=0;
+        for (int i=0;i<values.size();i++){
+            sum=sum+Integer.parseInt(values.get(i).getText());
+        }
+        System.out.println(sum);
     }
 }
