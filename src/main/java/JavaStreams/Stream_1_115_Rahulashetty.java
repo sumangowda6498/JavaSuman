52efc740f39cbd3f18a4928e12d3e86de86e7625
Suman <sumangowdaarbi@gmail.com>
2025-08-11 21:46:54 +0530

    Commit 11 August udemy Stream_1_115_Rahulashetty

--- src/main/java/JavaStreams/Stream_1_115_Rahulashetty.java
+++ src/main/java/JavaStreams/Stream_1_115_Rahulashetty.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 
 import javax.sound.midi.Soundbank;
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 public class Stream_1_115_Rahulashetty {
   // @Test
@@ -37,7 +38,27 @@ public class Stream_1_115_Rahulashetty {
        names.add("Akhil");
        names.add("Arjun");
 
+       //there is no life for intermediate operation if there is terminal operation
+       //terminal operation will execute only if inter opertion (filter ) returns true
+       //we can craete stream
+       //how to use filter in Stream API
+
       long c= names.stream().filter(s -> s.startsWith("A")).count();
        System.out.println("Count: "+c);
+
+//      long j= Stream.of("Abhijit","Suman","Sandesh","Arjun").filter(s->s.startsWith("A")).count();
+//       System.out.println("count j: "+j); or below line
+     long d= Stream.of("Abhijit","Suman","Sandesh","Arjun").filter(s->
+       {
+           s.startsWith("A");
+           return true;// if return false no count will be given
+       }).count();
+       System.out.println("Count d: "+d);
+
+
+       //print all the names of ArrayList which length is grater than 4
+       names.stream().filter(s->s.length()>4).forEach(s->System.out.println(s));
+       names.stream().filter(s->s.length()>4).limit(1).forEach(s->System.out.println(s));
+
    }
 }
