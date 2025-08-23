6a0bb16fb0a934f710a812a19ce12c689d89948d
Suman <sumangowdaarbi@gmail.com>
2025-08-24 00:00:07 +0530

    Commit 23 August udemy Stream_1_115_Rahulashetty

--- src/main/java/JavaStreams/Stream_1_115_Rahulashetty.java
+++ src/main/java/JavaStreams/Stream_1_115_Rahulashetty.java
@@ -8,6 +8,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Stream_1_115_Rahulashetty {
@@ -66,7 +67,7 @@ public class Stream_1_115_Rahulashetty {
 
    }
 
-@Test
+//@Test
    public void streamMap(){
         //print the names which has the last letter as "n" with uppercase
         Stream.of("Abhijit","Suman","Sandesh","Arjun").filter(s->s.endsWith("n")).map(s->s.toUpperCase()).forEach(s-> System.out.println(s));
@@ -87,5 +88,25 @@ public class Stream_1_115_Rahulashetty {
     boolean flag=newStream.anyMatch(s->s.equalsIgnoreCase("Ad77am"));
     System.out.println(flag);
     Assert.assertTrue(flag);
+    }
+
+    @Test
+    public void streamcollect(){
+       List<String> ls= Stream.of("Abhijata","Suman","Sandesh","Arjun","Rama").filter(s->s.endsWith("a")).map(s -> s.toUpperCase())
+               .collect(Collectors.toList());//.toSet() will covert to Set
+        System.out.println(ls.get(0));
+        System.out.print(ls);
+
+        List<Integer> number=Arrays.asList(2,2,3,4,1,7,8,9,8,5,8);
+        number.stream().distinct().forEach(s->System.out.print(s+" "));//print unique numbers
+        System.out.println();
+        List<Integer> li= number.stream().distinct().sorted().collect(Collectors.toList());
+        System.out.print(li.get(2)+" ");//
+        System.out.println();
+        System.out.println(li);
+
+
+
+
     }
 }
