817205b50aa44e87f85caa1a624b04191788fc70
Suman <sumangowdaarbi@gmail.com>
2025-08-29 23:52:47 +0530

    Commit 29 August udemy Stream_1_120_Rahulashetty

---
+++ src/main/java/UdemyRahul/Locator/Filter_WebElementStream_120.java
@@ -0,0 +1,28 @@
+package UdemyRahul.Locator;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Filter_WebElementStream_120 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.edge.driver","resources/msedgedriver.exe");
+        WebDriver driver=new EdgeDriver();
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.id("search-field")).sendKeys("Rice");
+        List<WebElement> veggeis=driver.findElements(By.xpath("//tr/td[1]"));
+
+        //5 results
+       List<WebElement> filteredList= veggeis.stream().filter(veggie->veggie.getText().contains("Rice")).collect(Collectors.toList());
+
+      Assert.assertEquals(veggeis.size(),filteredList.size());
+
+    }
+}
