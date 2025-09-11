21dfee3c0ba5ed62c1e7d579e39ae9108903c8d6
Suman <sumangowdaarbi@gmail.com>
2025-09-11 23:27:47 +0530

    Commit 11 September udemy TestNg_1_126_Rahulashetty CApture

---
+++ src/main/java/UdemyRahul/TestNg/day3_132_prioritizingTest.java
@@ -0,0 +1,20 @@
+package UdemyRahul.TestNg;
+
+import org.testng.annotations.Test;
+
+public class day3_132_prioritizingTest {
+    @Test
+    public  void webLoginCarLoan(){
+        System.out.println("WebLogin");
+    }
+
+    @Test
+    public void mobileLoginCarLoan(){
+        System.out.println("mobileLogin");
+    }
+
+    @Test
+    public void apiLoginCarLoan(){
+        System.out.println("apilogin");
+    }
+}
---
+++ src/main/java/UdemyRahul/TestNg/day3_homeloan.java
@@ -0,0 +1,20 @@
+package UdemyRahul.TestNg;
+
+import org.testng.annotations.Test;
+
+public class day3_homeloan {
+    @Test
+    public  void webLoginHomeLoan(){
+        System.out.println("HomeWebLogin");
+    }
+
+    @Test
+    public void mobileLoginCHomeLoan(){
+        System.out.println("HomemobileLogin");
+    }
+
+    @Test
+    public void apiLoginHomeLoan(){
+        System.out.println("Homeapilogin");
+    }
+}
